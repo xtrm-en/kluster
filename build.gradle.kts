817269de94546f31032b890c5ecaaf2ff22de8f5
xtrm-en <xtram.dev@gmail.com>
2021-10-07 19:10:48 +0200

    Initial commit.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.10" apply false
+    java
 }
 
 subprojects {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,8 @@
 rootProject.name = "kluster"
-
+include("interfaces")
+include("shared")
+include("callbookext")
+include("clientengine")
+include("clientui")
+include("engine")
+include("server")
